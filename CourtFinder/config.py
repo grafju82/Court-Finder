2a58ca5dd2cb8c3d0548e34a2aa465a757b0a590
Mike <34388502+mzacierka@users.noreply.github.com>
2019-03-28 13:44:42 -0400

    Update config.py

--- CourtFinder/config.py
+++ CourtFinder/config.py
@@ -9,4 +9,4 @@ class Config:
     DB_PASS = os.environ.get('DB_PASS')
 
     # Connection to Postgres server
-    #SQLALCHEMY_DATABASE_URI = 'mysql://' + DB_USER + ':' + DB_PASS + '@192.155.88.112:3306/courtfinder'
+    SQLALCHEMY_DATABASE_URI = 'mysql://' + DB_USER + ':' + DB_PASS + '@192.155.88.112:3306/courtfinder'
